5434189fb9641bbe3e4333c0a41affd2a8a55813
amirazzam2000 <45884568+amirazzam2000@users.noreply.github.com>
2019-12-15 23:19:29 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // app_id : 151d4902 , app_key : 771e0a32ea2b2631b0ad21f66a4ea564
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.tmb.cat/v1/transit/parades/2775?app_id=151d4902&app_key=771e0a32ea2b2631b0ad21f66a4ea564")
+                .build();
+        Response response;
+
+        try {
+            response = client.newCall(request).execute();
+            String jsonData = null;
+            if (response.body() != null){
+                jsonData = response.body().string();
+            }
+
+            System.out.println(jsonData);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
