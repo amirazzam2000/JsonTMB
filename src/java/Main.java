5db21b5045a36e7e4ccbc8408e5c5a190f06703a
amirazzam2000 <45884568+amirazzam2000@users.noreply.github.com>
2019-12-28 13:31:28 +0100

    Registering a new user

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,15 +1,7 @@
-import com.google.gson.Gson;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import System.MainSystem;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        MainSystem.mainSystem();
     }
 }
--- src/java/System/MainSystem.java
+++ src/java/System/MainSystem.java
@@ -7,8 +7,19 @@ import java.util.Scanner;
 public class MainSystem {
     public static void mainSystem(){
         UserManager users = new UserManager();
-        Scanner userName = new Scanner(System.in);
+        Scanner userScanner = new Scanner(System.in);
 
+        UI.printWelcomeMessage();
 
+        System.out.println("Username:");
+        users.setName(userScanner.nextLine());
+
+        System.out.println(System.lineSeparator() + "E-mail:");
+        users.setEmail(userScanner.nextLine());
+
+        System.out.println(System.lineSeparator() + "Birth Year:");
+        users.setYear(userScanner.nextInt());
+
+        UI.printInfoValidMessage();
     }
 }
