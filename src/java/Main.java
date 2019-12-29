825db5b77922aef2edc7d45080c8c76ef5adb39c
amirazzam2000 <45884568+amirazzam2000@users.noreply.github.com>
2019-12-29 13:44:53 +0100

    menu

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,7 +1,15 @@
+import API.WebManager;
+import JsonParsing.Location.JsonLocationReader;
+import JsonParsing.Transportation.JsonStationReader;
 import System.MainSystem;
 
 public class Main {
     public static void main(String[] args) {
+        String JsonString;
+        JsonLocationReader.readLocations();
+        JsonString = WebManager.callAllStations();
+        if (JsonString != null)
+            JsonStationReader.readStations(JsonString);
         MainSystem.mainSystem();
     }
 }
